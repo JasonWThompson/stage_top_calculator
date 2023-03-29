f993de6aa16d3247fad83e644a7ffd0eb24d2c90
Jason W. Thompson <jason@jasonandmary.com>
2023-03-28 22:47:49 -0500

    Added interactive UI

--- scripts.js
+++ scripts.js
@@ -1,9 +1,75 @@
+const canvas = document.getElementById('table-layout');
+const ctx = canvas.getContext('2d');
+let width = 2;
+let length = 2;
+let frameSize = 80;
+let offsetX = 10;
+let offsetY = 10;
+let maxSize = 8;
+
+function refreshTable() {
+    drawTableLayout();
+    calculateTableComponents();
+    updateButtonStyles();
+}
+
+function modifyTable(deltaWidth, deltaLength) {
+    width += deltaWidth;
+    length += deltaLength;
+
+    if (width < 2) width = 2;
+    if (length < 2) length = 2;
+    if (width > maxSize) width = maxSize;
+    if (length > maxSize) length = maxSize;
+
+    refreshTable();
+}
+
+function drawTableLayout() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (let x = 0; x < width; x++) {
+        for (let y = 0; y < length; y++) {
+            ctx.strokeRect(offsetX + x * frameSize, offsetY + y * frameSize, frameSize, frameSize);
+        }
+    }
+}
+
+function updateButtonStyles() {
+    let leftBtn = document.querySelector('.left-btn.fa-minus-square');
+    let rightBtn = document.querySelector('.right-btn.fa-plus-square');
+    let topBtn = document.querySelector('.top-btn.fa-minus-square');
+    let bottomBtn = document.querySelector('.bottom-btn.fa-plus-square');
+    
+    // For width
+    if (width === 2) {
+        leftBtn.style.opacity = "0.3";
+    } else {
+        leftBtn.style.opacity = "1";
+    }
+
+    if (width === maxSize) {
+        rightBtn.style.opacity = "0.3";
+    } else {
+        rightBtn.style.opacity = "1";
+    }
+
+    // For length
+    if (length === 2) {
+        topBtn.style.opacity = "0.3";
+    } else {
+        topBtn.style.opacity = "1";
+    }
+
+    if (length === maxSize) {
+        bottomBtn.style.opacity = "0.3";
+    } else {
+        bottomBtn.style.opacity = "1";
+    }
+}
+
 function calculateTableComponents() {
-    const width = parseInt(document.getElementById("width").value);
-    const length = parseInt(document.getElementById("length").value);
-
     const frames = width * length;
-
     const touching_edges = (width - 1) * length + width * (length - 1);
     const frame_locks = touching_edges * 2;
 
@@ -43,3 +109,8 @@ function calculateTableComponents() {
         <p>Tile Locks: ${total_tile_locks}</p>
     `;
 }
+
+refreshTable();
+drawTableLayout();
+calculateTableComponents();
+updateButtonStyles();
